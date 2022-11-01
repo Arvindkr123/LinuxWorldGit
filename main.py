e4cd5e7b2a3c4bd79b7317d87a9bcb83e5aee0ff
Arvind <thakurarvindkr10@gmail.com>
2022-11-01 09:53:00 +0530

    I have correct the main.py file on branch master by arvind kumar

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-print("Hello World");
-print("Hello there");
-print("Hello Ram");
-print("What's up");
+print("Hello World")
+print("Hello there")
+print("Hello Ram")
+print("What's up")
 
-name = input("Please Enter your name");
-print(f"your name is {name}");
+name = input("Please Enter your name")
+print(f"your name is {name}")
 
 # This is my comment here
 # kaho na pyar hai
-print("kaho na pyar hai");
-print("five line of the code here");
-print('Suno what are you doing");
+print("kaho na pyar hai")
+print("five line of the code here")
+print('Suno what are you doing')
 for i in range(1,11):
-	print(i);
-p
+	print(i)
+
 def Hello():
-return f""[DWhat's up boys and Girls";
+	return f"What\'s up boys and Girls";
 # Here is my hero comes to save me every time when he's neededEvery time you don't need to know who I am
-United state of America
-United state of Kingdom
-India
-Pakistan
-Canada
-and manymore place which i want to visit
+# United state of America
+# United state of Kingdom
+# India
+# Pakistan
+# Canada
+# and manymore place which i want to visit
